7eed13da6d5a6f431e053aba5f4b68a5420a5e90
kaushiktd <kaushiktd@gmail.com>
2022-10-18 12:43:37 +0530

    Search Insert Position Binary Search

--- leetcode/Algorithm/BinarySearch/Search_Insert_Position.py
+++ leetcode/Algorithm/BinarySearch/Search_Insert_Position.py
@@ -10,12 +10,21 @@ def searchInsertPosition(nums, target):
     return nums.index(target)
 
 
-def searchInsertPositionBS(num, t):
-
-
+def searchInsertPositionBS(numbers, t):
+    start, end = 0, len(numbers) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if numbers[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
 
 
 if __name__ == '__main__':
     nums = [1, 3, 5, 6]
     target = 5
-    print(searchInsertPosition(nums, target))
+    # print(searchInsertPosition(nums, target))
+    print(searchInsertPositionBS(nums, target))
+
+
