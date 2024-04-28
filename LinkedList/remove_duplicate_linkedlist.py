b9cc38cb6862858d0d47fe3d428697aa40292881
kaushiktd <kaushiktd@gmail.com>
2024-04-28 11:19:39 +0530

    Palindrom

--- LinkedList/remove_duplicate_linkedlist.py
+++ LinkedList/remove_duplicate_linkedlist.py
@@ -59,19 +59,70 @@ class LinkedList:
                 current_node = current_node.next
         return dummy.next
 
+    def remove_elements(self, value):
+        dummy = Node(-1)
+        dummy.next = self.head
+
+        prev = dummy
+        curr = self.head
+
+        while curr is not None:
+            if curr.value == value:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+
+        return dummy.next
+
+    def reverse_ll(self):
+        prev_node = None
+        current_node = self.head
+
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+
+        self.head = prev_node
+
+    def is_palindrome(self):
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        while prev:
+            if self.head.value != prev.value:
+                return False
+            self.head = self.head.next
+            prev = prev.next
+        return True
+
 
 
 if __name__ == '__main__':
     ll = LinkedList()
     ll.append(1)
     ll.append(2)
+    ll.append(3)
     ll.append(2)
     ll.append(3)
-    ll.append(4)
-    ll.append(5)
 
     print(ll)
 
-    ll.remove_dupes()
-    print(ll)
-
+    # ll.remove_dupes()
+    # print(ll)
+    # ll.remove_elements(2)
+    # print(ll)
+    # ll.reverse_ll()
+    # print(ll)
+    print(ll.is_palindrome())
