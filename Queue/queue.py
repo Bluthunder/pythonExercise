7caad21df23a457c4cc07ef26b89aea511947fc7
kaushiktd <kaushiktd@gmail.com>
2024-05-09 20:29:26 +0530

    Queue

---
+++ Queue/queue.py
@@ -0,0 +1,55 @@
+"""
+Date - 09.05.2024
+DSA - QUEUE
+
+This is ADT implementation of queue using python list (arrays) without any size limit
+
+"""
+
+
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
+
+    def is_empty(self):
+        if not self.items:
+            return True
+        else:
+            return False
+
+    def enqueue(self, value):
+        self.items.append(value)
+        return "element is inserted at end"
+
+    def dequeue(self):
+        if self.is_empty():
+            return " No element in Q"
+        else:
+            self.items.pop(0)
+
+    def peek(self):
+        if self.is_empty():
+            return "No Element in Q"
+        else:
+            return self.items[0]
+
+    def delete(self):
+        self.items = None
+
+
+if __name__ == '__main__':
+    customeQ = Queue()
+    print(customeQ.is_empty())
+    customeQ.enqueue(3)
+    customeQ.enqueue(2)
+    customeQ.enqueue(5)
+    customeQ.enqueue(6)
+    print(customeQ)
+    customeQ.dequeue()
+    print(customeQ)
+    print(customeQ.peek())
